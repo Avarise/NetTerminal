a9d97c95fb9b8bd681cfc10301fcb0576fab81fc
Avarise <sebakk123@gmail.com>
2022-06-06 15:50:17 +0200

    Add arduino source file

---
+++ NetTerminal.ino
@@ -0,0 +1,28 @@
+#include <ESP8266WiFi.h>
+
+struct AccessPointInfo {
+
+} apRegistry[SIZE_AP_REG];
+
+class RegistryManager {
+    private:
+
+    public:
+};
+
+/* 
+ * MessageBox class is implemented as a singleton with some simple I/O operations
+ * Alert
+ * Status
+ */
+class MessageBox {
+
+};
+
+void setup () {
+
+}
+
+void loop () {
+
+}
